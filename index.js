f8f803686c9b11a593a63f87eab5448214548ac1
JerickWong <jerick_wong@dlsu.edu.ph>
2021-09-12 21:13:45 +0800

    dynamic x domains

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const svg = d3.select("#my_dataviz")
 // x and x axis naman
 // made dynamically moving
 
-const y = d3.scaleBand()
+let y = d3.scaleBand()
   .range([ 0, height ])
 const yAxis = svg.append("g")
 
@@ -48,19 +48,15 @@ async function update() {
       let ti = {}
       if (types.length===1) {
         if (types[0].name==="Covid") {
-          ti.x = 0
-          ti.y = 1600000
           data = await d3.csv("/data/SEA Quarterly Confirmed COVID-19 Cases.csv")
         } else {
-          ti.x = -20
-          ti.y = 30
           data = await d3.csv("/data/SEA Quarterly GDP Growth Rate.csv")
         }
           
       }
 
       const x = d3.scaleLinear()
-      .domain([ti.x, ti.y])
+      .domain([d3.min(data, function(d) { return +d[quarters[0].name]; }), d3.max(data, function(d) { return +d[quarters[0].name]; })])
       .range([ 0, width]);
 
       svg.append("g")
@@ -86,7 +82,7 @@ async function update() {
       u.join("rect")
       .transition()
       .duration(1000)
-        .attr("x", x(ti.x))
+        .attr("x", x(d3.min(data, function(d) { return +d[quarters[0].name]; })))
         .attr("y", d => y(d.Country))
         .attr("width", d => x(d[quarters[0].name]))
         .attr("height", y.bandwidth())
@@ -103,6 +99,47 @@ async function update() {
       //   .attr("fill", "#5891ad")
     }
 }
+
+//Read the data
+// d3.csv("/data/SEA Quarterly Confirmed COVID-19 Cases.csv").then( function(data) {
+
+//   // group the data: I want to draw one line per group
+//   const sumstat = d3.group(data, d => d.Country); // nest function allows to group the calculation per level of a factor
+//   console.log(sumstat)
+//   // Add X axis --> it is a date format
+//   const x = d3.scaleLinear()
+//     .domain([d3.extent(data, function(d) { return d.year; })])
+//     .range([ 0, width ]);
+//   svg.append("g")
+//     .attr("transform", `translate(0, ${height})`)
+//     .call(d3.axisBottom(x).ticks(5));
+
+//   // Add Y axis
+//   const y = d3.scaleLinear()
+//     .domain([0, d3.max(data, function(d) { return +d.n; })])
+//     .range([ height, 0 ]);
+//   svg.append("g")
+//     .call(d3.axisLeft(y));
+
+//   // color palette
+//   const color = d3.scaleOrdinal()
+//     .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
+
+//   // Draw the line
+//   svg.selectAll(".line")
+//       .data(sumstat)
+//       .join("path")
+//         .attr("fill", "none")
+//         .attr("stroke", function(d){ return color(d[0]) })
+//         .attr("stroke-width", 1.5)
+//         .attr("d", function(d){
+//           return d3.line()
+//             .x(function(d) { return x(d.year); })
+//             .y(function(d) { return y(+d.n); })
+//             (d[1])
+//         })
+
+// })
 
 
 update()
