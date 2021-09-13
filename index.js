a8ce33a6894b9524e0581abeb738594ecc9a8536
JerickWong <jerick_wong@dlsu.edu.ph>
2021-09-14 02:42:32 +0800

    flex wrap

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .attr("style", "background-color: white;")
+    .attr("style", "background-color: white;;margin: 20px")
   .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
@@ -174,7 +174,74 @@ async function combinedGraph() {
     
 }
 
+async function multiGraph() {
+  //Read the data
+  d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv").then( function(data) {
+    
+    // group the data: I want to draw one line per group
+    const sumstat = d3.group(data, d => d.name) // nest function allows to group the calculation per level of a factor
 
+    // What is the list of groups?
+    let allKeys = new Set(data.map(d=>d.name))
+
+    // Add an svg element for each group. The will be one beside each other and will go on the next row when no more room available
+    const svg = d3.select("#my_dataviz")
+      .selectAll("uniqueChart")
+      .data(sumstat)
+      .enter()
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("style", "background-color: white;margin: 20px")
+      .append("g")
+        .attr("transform", 
+              `translate(${margin.left},${margin.top})`);
+
+    // Add X axis --> it is a date format
+    const x = d3.scaleLinear()
+      .domain(d3.extent(data, function(d) { return d.year; }))
+      .range([ 0, width ]);
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x).ticks(3));
+
+    //Add Y axis
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.n; })])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y).ticks(5));
+
+    // color palette
+    const color = d3.scaleOrdinal()
+      //.domain(allKeys)
+      .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
+
+    // Draw the line
+    svg
+      .append("path")
+        .attr("fill", "none")
+        .attr("stroke", function(d){ return color(d[0]) })
+        .attr("stroke-width", 1.9)
+        .attr("d", function(d){
+          return d3.line()
+            .x(function(d) { return x(d.year); })
+            .y(function(d) { return y(+d.n); })
+            (d[1])
+        })
+
+    // Add titles
+    svg
+      .append("text")
+      .attr("text-anchor", "start")
+      .attr("y", -5)
+      .attr("x", 0)
+      .text(function(d){ return(d[0])})
+      .style("fill", function(d){ return color(d[0]) })
+
+  })
+}
 
 update()
 
