54a6604487c8c2f054627d08abd26f4f2b30349c
JerickWong <jerick_wong@dlsu.edu.ph>
2021-09-16 21:48:49 +0800

    bigger graphs

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ inputs.forEach(input => {
 
 // set the dimensions and margins of the graph
 const margin = {top: 20, right: 30, bottom: 40, left: 90},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 530 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 let svg = d3.select("#my_dataviz")
